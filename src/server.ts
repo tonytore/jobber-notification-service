aed25c3ec5877ee6e9978e7d42e315ecd2f6a169
tonytor <teme4842@gmail.com>
2026-01-21 13:59:48 +0300

    feat: Add initial notification service server setup and logging, and ignore .npmrc in gitignore.

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import 'express-async-errors'
+import http from 'http';
+import { winstonLogger } from '@tonytore/jobber-shared';
+import appConfig from './config';
+import { Logger } from 'winston';
+import { Application } from 'express';
+
+
+const SERVER_PORT = process.env.PORT || 4001;
+
+const options = {
+    serviceName: "NotificationServer",
+    level: 'debug',
+    lokiUrl: appConfig.LOKI_URL,
+    enableLoki: true
+}
+
+const logger:Logger = winstonLogger(options);
+
+export function start(app: Application): void {
+    startServer(app);
+    startQueues();
+    startLoki();
+
+}
+
+async function startQueues(): Promise<void> {
+}
+
+function startLoki(): void {
+}
+
+function startServer(app:Application): void {
+    try {
+        const httpServer:http.Server = http.createServer(app);
+        logger.info(`Worker with process id og ${process.pid} on notification server has started`);
+        httpServer.listen(SERVER_PORT, () => {
+            logger.info(`Notification server listening on port ${SERVER_PORT}`);
+        });
+    } catch (error) {
+        logger.log('error', 'NotificationService startServer()', error);
+        
+    }
+}
